bc37947ac02702fac3cd55f5da479e9f7add6bcc
UsamaHameed <usamatech2@gmail.com>
2021-03-17 22:47:44 +0100

    destructure struct

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -19,8 +19,8 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+pub fn run(Config { filename, .. }: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(filename)?;
 
     println!("With text:\n{}", contents);
 
