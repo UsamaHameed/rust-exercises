577b902cf6b29f627101836280643f645f51b3b0
UsamaHameed <usamatech2@gmail.com>
2021-03-18 21:41:08 +0100

    add case insensitive option

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,9 @@
-use std::error::Error;
-use std::fs;
+use std::{fs, env, error::Error};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,15 +14,22 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
-pub fn run(Config { filename, query }: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(Config { filename, query, case_sensitive }: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(filename)?;
 
-    for line in search(&query, &contents) {
+    let results = if case_sensitive {
+        search(&query, &contents)
+    } else {
+        search_case_insensitive(&query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -43,6 +50,21 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -50,6 +72,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
