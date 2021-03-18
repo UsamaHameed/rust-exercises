f694fc083ed0d405d920d35766568fc11c6612a8
UsamaHameed <usamatech2@gmail.com>
2021-03-18 20:47:35 +0100

    add search functionality

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -7,7 +7,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -19,10 +19,40 @@ impl Config {
     }
 }
 
-pub fn run(Config { filename, .. }: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(Config { filename, query }: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
